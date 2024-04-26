4082ba77383b7758e29e420d9e30f63389edc6ed
charley-v <78202354+charley-v@users.noreply.github.com>
2024-04-26 12:16:32 -0700

    Update TuffyNav.py

--- TuffyNav.py
+++ TuffyNav.py
@@ -19,8 +19,8 @@ def click_marker_event(marker):
 class App(customtkinter.CTk):
 
     APP_NAME = "TuffyNav"
-    WIDTH = 800
-    HEIGHT = 500
+    WIDTH = 1920
+    HEIGHT = 1080
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -83,22 +83,72 @@ class App(customtkinter.CTk):
         self.map_widget.grid(row=1, rowspan=1, column=0, columnspan=3, sticky="nswe", padx=(0, 0), pady=(0, 0))
         
         
-        image_1 = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "arbor.jpg")).resize((300, 200)))        
-        marker_1 = self.map_widget.set_marker(33.88844,-117.88433, text="arboretum", image=image_1,image_zoom_visibility=(0, float("inf")), command=click_marker_event)
-        marker_1.hide_image(True)
-        image_2 = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "nutwood.jpg")).resize((300, 200)))        
-        marker_2 = self.map_widget.set_marker(33.87930,-117.88858, text="nutwood parking structure", image=image_2,image_zoom_visibility=(0, float("inf")), command=click_marker_event)
-        marker_2.hide_image(True)
-        
+        #image_1 = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "arbor.jpg")).resize((300, 200)))        
+        #marker_1 = self.map_widget.set_marker(33.88844,-117.88433, text="arboretum", image=image_1,image_zoom_visibility=(0, float("inf")), command=click_marker_event)
+        #marker_1.hide_image(True)
+        #image_2 = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "nutwood.jpg")).resize((300, 200)))        
+        #marker_2 = self.map_widget.set_marker(33.87930,-117.88858, text="nutwood parking structure", image=image_2,image_zoom_visibility=(0, float("inf")), command=click_marker_event)
+        #marker_2.hide_image(True)
         #defualt paths between nodes
-        path_1 = self.map_widget.set_path([marker_2.position, marker_1.position])#add color= '#FF0000' (red color) argument for when we print the specified path determined by an algo
-
-
+        #path_1 = self.map_widget.set_path([marker_2.position, marker_1.position])#add color= '#FF0000' (red color) argument for when we print the specified path determined by an algo
         #serch bar and search button, commented out since we probably wont use it
         #self.entry = customtkinter.CTkEntry(master=self.frame_right, placeholder_text="type address")
         #self.entry.grid(row=0, column=0, sticky="we", padx=(12, 0), pady=12)
         #self.entry.bind("<Return>", self.search_event)
 
+        image_1 = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "pin.png")).resize((35, 35)))        
+        marker_1 = self.map_widget.set_marker(33.878809374053155, -117.88341836262659, text = "Mihaylo Hall", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_2 = self.map_widget.set_marker(33.879065171017324, -117.88435302604127, text = "Langsdorf Hall", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_3 = self.map_widget.set_marker(33.879743528510005, -117.88413979569506, text = "University Hall", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_4 = self.map_widget.set_marker(33.879645405637, -117.88551155450016, text = "McCarthy Hall", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_5 = self.map_widget.set_marker(33.879278246672776, -117.88585654819984, text= "Dan Black Hall", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_6 = self.map_widget.set_marker(33.88059975992103, -117.88665536468746,  text= "Art Center", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_7 = self.map_widget.set_marker(33.880589167555, -117.8841163633055, text= "H-S Sciences", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_8 = self.map_widget.set_marker(33.88137321024358, -117.88434730161424, text= "EC", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_9 = self.map_widget.set_marker(33.88131806495993, -117.88525384316719, text = "Pollak Library", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_10 = self.map_widget.set_marker(33.88022688609634, -117.88863635944018, text = "Visual Arts Building", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_11 = self.map_widget.set_marker(33.88186676883376, -117.88845279258894, text = "TSU", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_12= self.map_widget.set_marker(33.882310882303656, -117.8830018704961, text = "ECS", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_13= self.map_widget.set_marker(33.88335255103566, -117.88782583780849, text = "REC Center", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_14 = self.map_widget.set_marker(33.882798357595945, -117.8861484851946, text = "Gym", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        marker_15 = self.map_widget.set_marker(33.88315934073165, -117.884411671363, text = "Health Center", icon = image_1, font=("Helvetica Bold", 12), text_color="black")
+        #defualt paths between nodes
+        path_1 = self.map_widget.set_path([marker_1.position, marker_3.position], width=5)#add color= '#FF0000' (red color) argument for when we print the specified path determined by an algo
+        path_2 = self.map_widget.set_path([marker_1.position, marker_2.position],width=5)
+        path_3 = self.map_widget.set_path([marker_2.position, marker_3.position],width=5)
+        path_4 = self.map_widget.set_path([marker_2.position, marker_5.position],width=5)
+        path_5 = self.map_widget.set_path([marker_2.position, marker_4.position],width=5)
+        path_6 = self.map_widget.set_path([marker_3.position, marker_4.position],width=5)
+        path_7 = self.map_widget.set_path([marker_3.position, marker_7.position],width=5)
+        path_8 = self.map_widget.set_path([marker_4.position, marker_6.position],width=5)
+        path_9 = self.map_widget.set_path([marker_4.position, marker_5.position],width=5)
+        path_10 = self.map_widget.set_path([marker_4.position, marker_9.position],width=5)
+        path_11 = self.map_widget.set_path([marker_4.position, marker_7.position],width=5)
+        path_12= self.map_widget.set_path([marker_10.position, marker_11.position],width=5)
+        path_13 = self.map_widget.set_path([marker_10.position, marker_6.position],width=5)
+        path_14 = self.map_widget.set_path([marker_6.position, marker_9.position],width=5)
+        path_15 = self.map_widget.set_path([marker_6.position, marker_8.position],width=5)
+        path_16= self.map_widget.set_path([marker_6.position, marker_7.position],width=5)
+        path_17 = self.map_widget.set_path([marker_8.position, marker_12.position],width=5)
+        path_18 = self.map_widget.set_path([marker_9.position, marker_15.position],width=5)
+        path_19 = self.map_widget.set_path([marker_9.position, marker_14.position],width=5)
+        path_20 = self.map_widget.set_path([marker_9.position, marker_13.position],width=5)
+        path_21 = self.map_widget.set_path([marker_9.position, marker_11.position],width=5)
+        path_22 = self.map_widget.set_path([marker_11.position, marker_13.position],width=5)
+        path_23 = self.map_widget.set_path([marker_13.position, marker_14.position],width=5)
+        path_24 = self.map_widget.set_path([marker_14.position, marker_15.position],width=5)
+        path_25= self.map_widget.set_path([marker_15.position, marker_12.position],width=5)
+        path_26= self.map_widget.set_path([marker_7.position, marker_8.position],width=5)
+
+        #test red path
+        path_5 = self.map_widget.set_path([marker_2.position, marker_4.position],width=10, color= 'red')
+        path_10 = self.map_widget.set_path([marker_4.position, marker_9.position],width=10, color = 'red')
+        path_19 = self.map_widget.set_path([marker_9.position, marker_14.position],width=10, color = 'red')
+
+
+
+
+        
         #self.button_search = customtkinter.CTkButton(master=self.frame_right,text="Search",width=90,command=self.search_event)
         #self.button_search.grid(row=0, column=1, sticky="w", padx=(12, 0), pady=12)
 
